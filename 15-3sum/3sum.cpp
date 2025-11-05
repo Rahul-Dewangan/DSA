1507f7b118a720ac1cf8eac8efb1c08299bc1ee6
Rahul Dewangan <96007595+Rahul-Dewangan@users.noreply.github.com>
2025-11-06 01:05:48 +0530

    Time: 77 ms (18.74%) | Memory: 37.4 MB (14.65%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -7,6 +7,7 @@ public:
         for (int i = 0; i < n-2; i++) {
             int tar = 0 - nums[i];
             int lo = i+1, hi = n - 1;
+            if(i>0 && nums[i]==nums[i-1])continue;
             while (lo < hi ) {
                 if (nums[lo] + nums[hi] == tar ) {
                     vector<int> res = {nums[i], nums[lo], nums[hi]};
