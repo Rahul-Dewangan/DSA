5d45d106e6fb3e007627cb2bacae2f612cbb3eb7
Rahul Dewangan <96007595+Rahul-Dewangan@users.noreply.github.com>
2025-12-03 23:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (6.03%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,38 +1,18 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        
-        lst = []
-        lst1 = []
-        low = 2**(2*n)
-        for i in range(low):
-            st = bin(i)[2:].zfill(2*n)
-            sum = 0
-            flag = True
-            res = ""
-            for c in st:
-                if c == '1':
-                    sum += 1
-                elif c == '0':
-                    sum += -1
 
-                if sum < 0:
-                    flag = False
-                    break
-
+        ans = []
+        def helper(low, high, seq):
+            if(len(seq) == 2*n):
+                ans.append(seq)
+                return
             
-            if(flag and sum == 0):
-                for c in st:
-                    if c == '1':
-                        res = res + '('
-                    elif c == '0':
-                        res = res +  ')'
-            
-            if len(res):
-                lst.append(res)
-                    
-
+            if(low < n):
+                helper(low+1, high, seq + '(')
+            if(high < low):
+                helper(low, high+1, seq + ')')
         
-        # print(lst)
+        helper(0, 0, '')
 
-        # print(str(bin(10))[2:])
-        return lst
+        return ans
+        
